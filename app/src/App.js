9d670e156196643e14c4e474857225069f5fd123
Etriz <ryanpdesigns@gmail.com>
2020-03-25 00:07:46 -0500

    jumbotron component

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <h1>ShoppingApp</h1>
+      <Nav />
+      <Header />
     </div>
   );
 }
---
+++ app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <main>
+      <section className="jumbo" alt="head">
+        <div className="container shop">
+          <h1>Furniture Collection</h1>
+          <button className="btn">Shop Now</button>
+        </div>
+      </section>
+    </main>
+  );
+}
