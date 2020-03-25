94358d7b599d94b4b6bb188ddcb739f38ecffdfe
Etriz <ryanpdesigns@gmail.com>
2020-03-25 00:07:20 -0500

    nav component

---
+++ app/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <nav>
+      <div className="container">
+        <span className="menuBtn">Menu</span>
+        <h1 className="logo">HomeHideaway</h1>
+        <span className="cartBtn">Cart</span>
+      </div>
+    </nav>
+  );
+}
