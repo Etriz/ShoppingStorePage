13e2981e82096a11b3b3afb353d59225ef7b001a
Etriz <ryanpdesigns@gmail.com>
2020-03-26 23:53:40 -0500

    map cards over local JSON data - no images

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import faker from "faker";
 import ProductCard from "./ProductCard";
+import products from "../products.json";
 
 export default function Header() {
   // let testFake = faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}");
@@ -8,15 +9,11 @@ export default function Header() {
   const createProduct = () => {
     const fakeDesc = faker.lorem.sentences();
     const fakePrice = faker.commerce.price();
-    // log the fake data just to make sure we get results
-    // console.log({ src: fakeImg, name: fakeName, desc: fakeDesc, price: fakePrice });
 
     return { desc: fakeDesc, price: fakePrice };
   };
 
-  return (
-    <main className="container">
-      <ProductCard createProduct={createProduct} />
-    </main>
-  );
+  const cardData = products.items.map(product => <ProductCard product={product} />);
+
+  return <main className="container">{cardData}</main>;
 }
--- app/src/components/ProductCard.js
+++ app/src/components/ProductCard.js
@@ -1,25 +1,20 @@
 import React from "react";
-import products from "../products.json";
 
 export default function ProductCard(props) {
-  const data = products.items;
+  const data = props.product;
 
-  const productData = data.map(item => {
-    return (
-      <div className="card" key={item.sys.id}>
-        <img src={item.fields.image.fields.file.url} alt="product" />
-        <h1>{item.fields.title.toUpperCase()}</h1>
-        <p>{props.createProduct().desc}</p>
-        <div className="infoPanel">
-          <div className="btnGrp">
-            <button className="viewBtn">View</button>
-            <button className="cartBtn">Add To Cart</button>
-          </div>
-          <span className="price">Price: ${item.fields.price}</span>
+  return (
+    <div className="card" key={data.sys.id}>
+      <img src={data.fields.image.fields.file.url} alt="product" />
+      <h1>{data.fields.title.toUpperCase()}</h1>
+      {/* <p>{props.createProduct().desc}</p> */}
+      <div className="infoPanel">
+        <div className="btnGrp">
+          <button className="viewBtn">View</button>
+          <button className="cartBtn">Add To Cart</button>
         </div>
+        <span className="price">Price: ${data.fields.price}</span>
       </div>
-    );
-  });
-
-  return productData;
+    </div>
+  );
 }
