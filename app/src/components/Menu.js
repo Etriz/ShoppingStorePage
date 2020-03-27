922897ead654fbcb532449eeabd09196de6b0430
Etriz <ryanpdesigns@gmail.com>
2020-03-26 21:36:05 -0500

    productCard styles

--- app/src/components/Menu.js
+++ app/src/components/Menu.js
@@ -3,9 +3,15 @@ import React from "react";
 export default function Header() {
   return (
     <div className="menu container">
-      <a className="">Link 01</a>
-      <a className="">Link 02</a>
-      <a className="">Link 03</a>
+      <a href="index.html" className="">
+        Link 01
+      </a>
+      <a href="index.html" className="">
+        Link 02
+      </a>
+      <a href="index.html" className="">
+        Link 03
+      </a>
     </div>
   );
 }
--- app/src/components/ProductCard.js
+++ app/src/components/ProductCard.js
@@ -3,15 +3,15 @@ import React from "react";
 export default function ProductCard(props) {
   return (
     <div className="card">
-      <h1>{props.createProduct().name}</h1>
       <img src={props.createProduct().src} alt="product" />
+      <h1>{props.createProduct().name}</h1>
       <p>{props.createProduct().desc}</p>
       <div className="infoPanel">
         <div className="btnGrp">
           <button>View</button>
           <button>Add To Cart</button>
         </div>
-        <span>Price:{props.createProduct().price}</span>
+        <span>Price: ${props.createProduct().price}</span>
       </div>
     </div>
   );
