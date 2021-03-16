7c366c25605fa4096a8cf1bc41ad4a914c96b81e
ryanp <ryanpanimation@gmail.com>
2021-03-16 00:49:33 -0500

    update package dependency versions

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,21 +1,12 @@
-import React, { useContext } from "react";
-import storeContext from "../contexts/StoreContext";
+import React, { useContext } from 'react';
+import storeContext from '../contexts/StoreContext';
 
 // import faker from "faker";
-import ProductCard from "./ProductCard";
-import productsJSON from "../products.json";
+import ProductCard from './ProductCard';
+import productsJSON from '../products.json';
 
 export default function Main() {
   const { cartContents, setCartContents } = useContext(storeContext);
-
-  // let testFake = faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}");
-
-  // const createProduct = () => {
-  //   const fakeDesc = faker.lorem.sentences();
-  //   const fakePrice = faker.commerce.price();
-
-  //   return { desc: fakeDesc, price: fakePrice };
-  // };
 
   const cardData = productsJSON.items.map((item) => (
     <ProductCard
--- app/src/components/ProductCard.js
+++ app/src/components/ProductCard.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Modal from "../components/Modal";
+import React from 'react';
+// import Modal from "../components/Modal";
 
 export default function ProductCard(props) {
   const product = props.product;
